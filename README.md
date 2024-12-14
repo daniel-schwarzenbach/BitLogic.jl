aa5fb0e674506e4e47ba64859b6ae59117f541d6
Daniel Schwarzenbach <dschwarze@student.ethz.ch>
2024-12-14 21:03:04 +0100

    Updated Readme

--- README.md
+++ README.md
@@ -1,3 +1,53 @@
 # BitLogic
 
-[![Build Status](https://github.com/daniel-schwarzenbach/BitLogic.jl/actions/workflows/CI.yml/badge.svg?branch=main)](https://github.com/daniel-schwarzenbach/BitLogic.jl/actions/workflows/CI.yml?query=branch%3Amain)
+The BitLogic module provides a custom `Bit` type and implements standard logical operators for bitwise operations. 
+It allows for logical operations using a `Bit` type, which is a subtype of `Integer` and can represent boolean values 
+as `0` and `1`. This module is designed to be used as a replacement for the `Bool` type in logical expressions.
+
+# Features
+
+- **Bit Type**: A custom type `Bit` that can be initialized with `0`, `1`, or `Bool` values (`true` or `false`).
+- **Logical Operators**: Implements standard logical operators such as `¬` (Not), `∧` (And), `∨` (Or), `↑` (Nand), `↓` (Nor), 
+  `⊕` (Xor), `→` (Right Implication), `←` (Left Implication), and `↔` (Equivalence).
+- **Truth Table Generation**: A function `truth_table` that generates and prints a truth table for a given logical function.
+- **Function Equivalence**: A function `≣` that checks if two logical functions produce the same output for all possible inputs.
+
+# Usage
+
+- Initialize `Bit` values:
+  ```julia
+  a = Bit(0)
+  b::Bit = 1
+  d::Bit = true
+  ```
+- Perform logical operations:
+```julia
+a ∧ b  # And operation
+a ∨ b  # Or operation
+¬a     # Not operation
+```
+- Generate a truth table for a logical function:
+```julia
+f(a, b) = a → ¬b
+println(truth_table(f))
+```
+output:
+```
+f(0, 0) = 1
+f(1, 0) = 1
+f(0, 1) = 1
+f(1, 1) = 0
+```
+- Check equivalence of two functions:
+```julia
+f1(a, b) = a ∧ b
+f2(a, b) = b ∧ a
+f1 ≣ f2  # true
+```
+saddly the `≡` is already in use in Julia, but one can just overload it manually if one wishes:
+```julia
+≡ = ≣
+f1 ≡ f2 # true
+```
+This module is useful for applications requiring custom logical 
+    operations and truth table analysis.
--- src/BitLogic.jl
+++ src/BitLogic.jl
@@ -1,5 +1,46 @@
+"""
+The BitLogic module provides a custom `Bit` type and implements standard logical operators for bitwise operations. 
+It allows for logical operations using a `Bit` type, which is a subtype of `Integer` and can represent boolean values 
+as `0` and `1`. This module is designed to be used as a replacement for the `Bool` type in logical expressions.
 
+# Features
+
+- **Bit Type**: A custom type `Bit` that can be initialized with `0`, `1`, or `Bool` values (`true` or `false`).
+- **Logical Operators**: Implements standard logical operators such as `¬` (Not), `∧` (And), `∨` (Or), `↑` (Nand), `↓` (Nor), 
+  `⊕` (Xor), `→` (Right Implication), `←` (Left Implication), and `↔` (Equivalence).
+- **Truth Table Generation**: A function `truth_table` that generates and prints a truth table for a given logical function.
+- **Function Equivalence**: A function `≣` that checks if two logical functions produce the same output for all possible inputs.
+
+# Usage
+
+- Initialize `Bit` values:
+  ```julia
+  a = Bit(0)
+  b::Bit = 1
+  d::Bit = true
+  ```
+- Perform logical operations:
+```
+a ∧ b  # And operation
+a ∨ b  # Or operation
+¬a     # Not operation
+```
+- Generate a truth table for a logical function:
+```
+f(a, b) = a → ¬b
+println(truth_table(f))
+```
+- Check equivalence of two functions:
+```
+f1(a, b) = a ∧ b
+f2(a, b) = b ∧ a
+println(f1 ≣ f2)  # true
+```
+This module is useful for applications requiring custom logical 
+    operations and truth table analysis.
+"""
 module BitLogic
+
 
 export Bit, ¬, ∧, ∨, ↑, ↓, ⊕, →, ←, ↔, truth_table, ≣
 
@@ -171,22 +212,22 @@ export truth_table
 retruns true only if for all inputs f1 and f2 you get the same output
 """
 function ≣(f1::Function, f2::Function) ::Bool
-    # Get the number of arguments for the first method of each function
-    n = length(first(methods(f1)).sig.parameters) - 1
-    n2 = length(first(methods(f2)).sig.parameters) - 1
+        # Get the number of arguments for the first method of each function
+        n = length(first(methods(f1)).sig.parameters) - 1
+        n2 = length(first(methods(f2)).sig.parameters) - 1
 
-    # Check if the number of arguments is the same
-    if n != n2
-        return false
+        # Check if the number of arguments is the same
+        if n != n2
+            return false
+        end
+    # Generate all combinations of true/false for `n` arguments
+    combinations = Iterators.product(fill((Bit(0), Bit(1)), n)...)
+    for inputs in combinations
+        if f1(inputs...) != f2(inputs...)
+            return false
+        end
     end
-   # Generate all combinations of true/false for `n` arguments
-   combinations = Iterators.product(fill((Bit(0), Bit(1)), n)...)
-   for inputs in combinations
-       if f1(inputs...) != f2(inputs...)
-           return false
-       end
-   end
-   return true
+    return true
 end
 
 end
